f26182baeae0da0f4ff18c50cee42b6972f8e71b
Anderson <ilesanderson01@gmail.com>
2026-01-19 11:35:57 -0500

    Intengo número 6 de que funcione el request Posta la ruta de la Api_Auth

--- src/config/database.js
+++ src/config/database.js
@@ -3,8 +3,7 @@ require('dotenv').config();
 
 const pool =new Pool({
     connectionString:process.env.DATABASE_URL_AUTH_DB,
-    ssl:process.env.NODE_ENV==='production'?{nojectUnauthorized:true}:false
-//    connectionString:process.env.DATABASE_URL_AUTH_DB
+    ssl:process.env.NODE_ENV==='production'?{rejectUnauthorized:false}:false
 });
 
 module.exports=pool;
